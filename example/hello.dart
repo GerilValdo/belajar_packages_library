b5e7f2e4ca8c2396b4be00bc4c535cdadd1e6774
Geril Valdo <gerilmanday@gmail.com>
2024-06-28 14:19:34 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,10 @@
+import 'package:belajar_packages_library/hello.dart';
+
+void main(){
+  print(sayHello('Geril'));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+
+}
